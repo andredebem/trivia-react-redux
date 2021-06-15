0608e8ef6f3af0a94206af6abf5bd00910e902b4
André De Bem <andrebemferraz@gmail.com>
2021-06-15 19:32:38 -0300

    Finalizado requisito 5

--- src/pages/Perguntas.jsx
+++ src/pages/Perguntas.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import md5 from 'crypto-js/md5';
-// import PropTypes from 'prop-types'
-// Iniciando Requisito 5
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class Perguntas extends Component {
+class Perguntas extends Component {
   constructor() {
     super();
 
@@ -11,10 +11,13 @@ export default class Perguntas extends Component {
       avatarLink: '',
       name: '',
       score: 0,
+      questions: [],
     };
 
     this.getImageGravatar = this.getImageGravatar.bind(this);
     this.getScoreAndName = this.getScoreAndName.bind(this);
+    this.fetchQuestions = this.fetchQuestions.bind(this);
+    this.renderQuestions = this.renderQuestions.bind(this);
   }
 
   componentDidMount() {
@@ -23,6 +26,7 @@ export default class Perguntas extends Component {
     const hashEmail = md5(gravatarEmail).toString();
     this.getImageGravatar(hashEmail);
     this.getScoreAndName(score, name);
+    this.fetchQuestions();
   }
 
   getImageGravatar(hashEmail) {
@@ -39,9 +43,61 @@ export default class Perguntas extends Component {
     });
   }
 
-  render() {
-    const { avatarLink, name, score } = this.state;
+  fetchQuestions() {
+    const { token } = this.props;
+    fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
+      .then((response) => response.json())
+      .then((object) => this.setState({
+        questions: object.results,
+      }));
+  }
 
+  renderQuestions() {
+    const { questions } = this.state;
+    return questions
+      .map(({
+        category,
+        question, correct_answer: correctAnswer,
+        incorrect_answers: incorrectAnswer },
+      index) => {
+        const magicNumber = 0.5;
+        const answers = (incorrectAnswer.concat(correctAnswer))
+          .sort(() => Math.random() - magicNumber);
+        // Referência da função de randomizar o array: https://flaviocopes.com/how-to-shuffle-array-javascript/
+        const renderAnswers = answers.map((answer, index2) => {
+          if (answer === correctAnswer) {
+            return (
+              <button
+                key={ answer }
+                type="button"
+                data-testid="correct-answer"
+              >
+                {answer}
+              </button>
+            );
+          }
+          return (
+            <button
+              key={ answer }
+              type="button"
+              data-testid={ `wrong-answer-${index2}` }
+            >
+              {answer}
+            </button>
+          );
+        });
+        return (
+          <div key={ index }>
+            <p key={ category } data-testid="question-category">{ category }</p>
+            <p key={ question } data-testid="question-text">{ question }</p>
+            { renderAnswers }
+          </div>
+        );
+      });
+  }
+
+  render() {
+    const { avatarLink, name, score, questions } = this.state;
     return (
       <div>
         <header>
@@ -53,7 +109,20 @@ export default class Perguntas extends Component {
           <span data-testid="header-player-name">{ name }</span>
           <span data-testid="header-score">{ score }</span>
         </header>
+        <main>
+          { questions.length > 0 && this.renderQuestions() }
+        </main>
       </div>
     );
   }
 }
+
+const mapStateTopProps = (state) => ({
+  token: state.token.token,
+});
+
+Perguntas.propTypes = {
+  token: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateTopProps, null)(Perguntas);
