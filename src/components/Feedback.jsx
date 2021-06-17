f73680b0a8b5a03d80fd803ffad77c7b722e58be
André De Bem <andrebemferraz@gmail.com>
2021-06-17 18:24:30 -0300

    Requisito 13 Finalizado

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export default class Feedback extends Component {
+  constructor() {
+    super();
+
+    this.renderMessage = this.renderMessage.bind(this);
+  }
+
+  renderMessage() {
+    const { correctQty } = this.props;
+    const THREE = 3;
+    if (correctQty < THREE) {
+      return 'Podia ser melhor...';
+    }
+    return 'Mandou bem!';
+  }
+
   render() {
     return (
-      <span data-testid="feedback-text">Mensagem de Feedback</span>
+      <div>
+        <span data-testid="feedback-text">{ this.renderMessage() }</span>
+      </div>
     );
   }
 }
+
+Feedback.propTypes = {
+  correctQty: PropTypes.number.isRequired,
+};
