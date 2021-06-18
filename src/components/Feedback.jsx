bfa630b35daf551c9a0cc8c47f4f1efdeb506061
André De Bem <78712956+andredebem@users.noreply.github.com>
2021-06-18 18:49:16 -0300

    Update src/components/Feedback.jsx

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -28,7 +28,7 @@ export default class Feedback extends Component {
         <Link to="/">
           <button type="button" data-testid="btn-play-again">Jogar novamente </button>
         </Link>
-        <Link to="/Ranking">
+        <Link to="/ranking">
           <button type="button" data-testid="btn-ranking">JRanking </button>
         </Link>
       </div>
